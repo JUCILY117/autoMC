4d800a0f6f0824bfcbad01fd49ee22d737b57583
JUCILY117 <111986929+JUCILY117@users.noreply.github.com>
2025-05-18 17:59:16 +0530

    added auto accept eula and added start script for linux systems

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
-@echo off
-echo Starting Minecraft server...
-if not exist eula.txt (
+#!/bin/bash
+
+echo "Starting Minecraft server..."
+
+if [ ! -f eula.txt ]; then
     echo "eula=true" > eula.txt
-)
+fi
+
 java -Xms3G -Xmx3G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Dfile.encoding=UTF-8 -jar paper.jar nogui
 
-echo Server has stopped. Running backup...
+echo "Server has stopped. Running backup..."
 node backup.js
 
-echo Backup complete. Press any key to exit.
-pause
+echo "Backup complete. Press any key to exit."
+read -n 1 -s -r
